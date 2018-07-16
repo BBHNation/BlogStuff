45c0a178df3bb83158255fd48ec222748fe5662d
baibinhan <baibinhan@cmiot.chinamobile.com>
2018-07-16 11:43:44 +0800

    前端VUE代码初始化

---
+++ blog/src/components/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-container class="container">
+    <el-aside class="aside">
+      <div style="position:absolute; top:50%; align:center; margin-left:auto; margin-right:auto; width:200px"> Aside
+      </div>
+    </el-aside>
+    <el-main class="main">
+      Main
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.container {
+  height: 100vh;
+}
+.aside {
+  background-color: #898989;
+  height: 100vh;
+  width: 200px;
+}
+.main {
+  background-color: #213123;
+  height: 100vh;
+}
+</style>
