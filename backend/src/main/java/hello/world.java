45263fd7a8158e8115e5dc66ff22af40d35bccba
Hancock <bai.szsy@qq.com>
2018-08-07 23:29:03 +0800

    add router

--- backend/src/main/java/hello/world.java
+++ backend/src/main/java/hello/world.java
@@ -1,14 +1,21 @@
 package hello;
 
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class world {
-    @RequestMapping
+    @RequestMapping(value = "/good", method = RequestMethod.GET)
     public String index() {
         return "hello world";
     }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test() {
+        return "test";
+    }
+
 }
 
 
--- backend/src/main/java/router/MainRouter.java
+++ backend/src/main/java/router/MainRouter.java
@@ -1,15 +1,21 @@
 package router;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.json.JSONObject;
 
 @RestController
 public class MainRouter {
-    @RequestMapping
-    public Map getBlogList() {
-        return new HashMap();
+    @RequestMapping(value = "/blog/getBlogList", method = RequestMethod.POST)
+    public JSONObject getBlogList() {
+        JSONObject object = new JSONObject();
+        object.put("code", 200);
+        return object;
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String test() {
+        return "index";
     }
 }
